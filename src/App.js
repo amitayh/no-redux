e9a5493e9b889e88e009c1187233d621eb24428b
Amitay Horwitz <amitayh@gmail.com>
2018-03-02 11:32:00 +0200

    Moved structure to App component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,52 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {PureComponent} from 'react';
+import 'todomvc-app-css/index.css';
 
-class App extends Component {
+class App extends PureComponent {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <section className="todoapp">
+        <header className="header">
+          <h1>todos</h1>
+          <input className="new-todo" placeholder="What needs to be done?" autofocus/>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <section className="main">
+          <input id="toggle-all" className="toggle-all" type="checkbox"/>
+          <label for="toggle-all">Mark all as complete</label>
+          <ul className="todo-list">
+            <li className="completed">
+              <div className="view">
+                <input className="toggle" type="checkbox" checked/>
+                <label>Taste JavaScript</label>
+                <button className="destroy"></button>
+              </div>
+              <input className="edit" value="Create a TodoMVC template"/>
+            </li>
+            <li>
+              <div className="view">
+                <input className="toggle" type="checkbox"/>
+                <label>Buy a unicorn</label>
+                <button className="destroy"></button>
+              </div>
+              <input className="edit" value="Rule the web"/>
+            </li>
+          </ul>
+        </section>
+        <footer className="footer">
+          <span className="todo-count"><strong>0</strong> item left</span>
+          <ul className="filters">
+            <li>
+              <a className="selected" href="#/">All</a>
+            </li>
+            <li>
+              <a href="#/active">Active</a>
+            </li>
+            <li>
+              <a href="#/completed">Completed</a>
+            </li>
+          </ul>
+          <button className="clear-completed">Clear completed</button>
+        </footer>
+      </section>
     );
   }
 }
